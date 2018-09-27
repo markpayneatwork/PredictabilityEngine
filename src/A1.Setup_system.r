ff19c4d4bc2de7cc395e63c8009392725fb1ae94
Mark Payne on HP Elitebook <mpay@aqua.dtu.dk>
2018-09-27 09:34:40 +0200

    A1: Moved common directories etc to PredEng package

--- src/A1.Setup_system.r
+++ src/A1.Setup_system.r
@@ -41,34 +41,6 @@ library(lubridate)
 install.packages("resources/ClimateTools", repos = NULL, type="source")
 install.packages("resources/PredEng_Package", repos = NULL, type="source")
 
-#/*======================================================================*/
-#  Define common directories etc
-#/*======================================================================*/
-PE.cfg <- list()
-PE.cfg$VOI.name <- "Variable_of_interest"
-PE.cfg$ROI.extraction.buffer <- 2  #Degrees
-PE.cfg$n.CMIP.chunks <- 5  #How many chunks should the CMIP5 ensemble be broken into?
-PE.cfg$NMME.epoch.start <- ymd("1960-01-01")
-
-#Directories
-PE.cfg$dirs <- list(Misc.meta="Z.Misc.meta",
-                    datasrc="data_srcs",
-                    cfg="scratch/Job_configuration")
-
-#File names
-PE.cfg$files <- list(Obs.monthly.anom.metadata=file.path(PE.cfg$dir$Misc.meta,"Monthly_anom_metadata.RData"),
-                     Obs.climatology.metadata=file.path(PE.cfg$dir$Misc.meta,"Climatology_metadata.RData"),
-                     fragment.meta=file.path(PE.cfg$dir$Misc.meta,"Fragment_metadata.RData"),
-                     fragstack.meta=file.path(PE.cfg$dir$Misc.meta,"Fragstack_metadata.RData"),
-                     anom.meta="Anomaly_metadata.RData",
-                     realmean.meta="Realmean_metadata.RData",
-                     analysis.grid="analysis.grid",
-                     remapping.wts="remapping_wts.nc",
-                     ensmean.name="Ensmean",
-                     regridded.landmask="Regridded_landmask.nc")
-
-save(PE.cfg,file="objects/PredEng_config.RData")
-
 #Turn off thte lights
 if(grepl("pdf|png|wmf",names(dev.cur()))) {dmp <- dev.off()}
 log_msg("\nAnalysis complete in %.1fs at %s.\n",proc.time()[3]-start.time,base::date())
