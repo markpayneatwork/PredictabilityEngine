36631b9537c654080e25b8dcf4a17ae735bd0297
Mark R. Payne - Aqua-CB-MPAY18 <mpay@aqua.dtu.dk>
2019-04-29 12:03:54 +0200

    Convert all saved files to .rds

--- src/X.PredEng_Package/R/Common_configuration.r
+++ src/X.PredEng_Package/R/Common_configuration.r
@@ -17,12 +17,12 @@ PE.cfg$dirs <- list(Misc.meta="Z.Misc.meta",
                     cfg="scratch/Job_configuration")
 
 #File names
-PE.cfg$files <- list(Obs.monthly.anom.metadata=file.path(PE.cfg$dir$Misc.meta,"Monthly_anom_metadata.RData"),
-                     Obs.climatology.metadata=file.path(PE.cfg$dir$Misc.meta,"Climatology_metadata.RData"),
-                     fragment.meta=file.path(PE.cfg$dir$Misc.meta,"Fragment_metadata.RData"),
-                     fragstack.meta=file.path(PE.cfg$dir$Misc.meta,"Fragstack_metadata.RData"),
-                     anom.meta="Anomaly_metadata.RData",
-                     realmean.meta="Realmean_metadata.RData",
+PE.cfg$files <- list(Obs.monthly.anom.metadata=file.path(PE.cfg$dir$Misc.meta,"Monthly_anom_metadata.rds"),
+                     Obs.climatology.metadata=file.path(PE.cfg$dir$Misc.meta,"Climatology_metadata.rds"),
+                     fragment.meta=file.path(PE.cfg$dir$Misc.meta,"Fragment_metadata.rds"),
+                     fragstack.meta=file.path(PE.cfg$dir$Misc.meta,"Fragstack_metadata.rds"),
+                     anom.meta="Anomaly_metadata.rds",
+                     realmean.meta="Realmean_metadata.rds",
                      analysis.grid="analysis.grid",
                      remapping.wts="remapping_wts.nc",
                      ensmean.name="Ensmean",
