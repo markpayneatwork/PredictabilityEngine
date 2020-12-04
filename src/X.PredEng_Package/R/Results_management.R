36e224aa9b461c4b2bbbeafd4383ec6e0d453e51
Mark R. Payne - Aqua-CB-MPAY18 <mpay@aqua.dtu.dk>
2020-12-04 11:09:59 +0100

    Added results.list.sd

--- src/X.PredEng_Package/R/Results_management.R
+++ src/X.PredEng_Package/R/Results_management.R
@@ -251,9 +251,20 @@ PE.db.getQuery <- function(pcfg,this.sql,silent=TRUE) {
 #' @param l List of raster layers
 #'
 #' @return
+#' @name rasterList
 #' @export
 raster.list.mean <- function(l) {
   l.stack <- raster::brick(l)
   l.mean <- raster::mean(l.stack) #Dispatching can be a bit strange here sometimes
   return(list(l.mean))
 }
+
+
+#' @export
+#' @rdname rasterList
+raster.list.sd <- function(l) {
+  l.stack <- raster::brick(l)
+  l.sd <- raster::calc(l.stack,sd) #Dispatching can be a bit strange here sometimes
+  return(list(l.sd))
+}
+
