6ad23686d47d1be7b3f3c4c090fcca2f5d18f254
Mark R. Payne - Aqua-CB-MPAY18 <mpay@aqua.dtu.dk>
2021-02-08 21:02:30 +0100

    Separate download of spatial metrics

--- manuscript/sync.sh
+++ manuscript/sync.sh
@@ -4,16 +4,25 @@
 
 #Configuration
 SRV=transfer:/zhome/39/c/38865/Predictability_engine/scratch/
-CFGS=("Bluefin" "Blue_whiting_decadal" "Mackerel_summer" "NA_Sal" "NA_SST")
 FILES_FROM=these.files
 
 rm $FILES_FROM
 
+
+CFGS=("Bluefin" "Blue_whiting_decadal" "Mackerel_summer")
 for CFG in "${CFGS[@]}"
 do
   echo "$CFG/configuration.rds" >> $FILES_FROM
   echo "$CFG/${CFG}_statistics.sqlite" >> $FILES_FROM
   echo "$CFG/${CFG}_metrics.sqlite" >> $FILES_FROM
+done
+
+CFGS=("NA_Sal" "NA_SST")
+
+for CFG in "${CFGS[@]}"
+do
+  echo "$CFG/configuration.rds" >> $FILES_FROM
+  echo "$CFG/${CFG}_metricsField.sqlite" >> $FILES_FROM
 done
 
 rsync -av --files-from=$FILES_FROM $SRV . --progress 
